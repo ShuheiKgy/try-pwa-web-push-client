d016e9851402aff290daa72d0ab0a36857d07df9
Shuhei Kagaya <shuhei.lab@gmail.com>
2019-05-11 23:58:19 +0900

    微修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     <h1>Try Web Push</h1>
+
+    <section>
+      <h2>Env Information</h2>
+      <p>
+        <span>Web Push</span>
+        <span>{{ webPushUsable | usable }}</span>
+      </p>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  filters: {
+    usable: flg => (flg === true ? "⭕️" : "❌")
+  },
+  computed: {
+    webPushUsable: vm => vm.$_webPush.usable
+  }
 };
 </script>
